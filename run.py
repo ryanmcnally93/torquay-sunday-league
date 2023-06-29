1dc6a8722e6cd8b6523d963a7cbcc6945afca365
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-06-29 15:56:22 +0000

    base.html can now view in port 5000

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from torquay_super_league import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
