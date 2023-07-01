d13ddee7d1c19266e2134b3dd8c2dc611a4966f2
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-07-01 20:04:55 +0000

    Add teams & add_team.html's to routes

--- torquay_sunday_league/routes.py
+++ torquay_sunday_league/routes.py
@@ -6,3 +6,13 @@ from torquay_sunday_league.models import Team, Player
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/teams")
+def teams():
+    return render_template("teams.html")
+
+
+@app.route("/add_team", methods=["GET", "POST"])
+def add_team():
+    return render_template("add_team.html")
