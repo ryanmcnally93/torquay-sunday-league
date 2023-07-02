fb83d0dca4423f441da0788e565ba5454e2ee59b
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-07-02 14:42:30 +0000

    Add POST functionality for create_team

--- torquay_sunday_league/routes.py
+++ torquay_sunday_league/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from torquay_sunday_league import app, db
 from torquay_sunday_league.models import Team, Player
 
@@ -13,6 +13,11 @@ def teams():
     return render_template("teams.html")
 
 
-@app.route("/add_team", methods=["GET", "POST"])
-def add_team():
-    return render_template("add_team.html")
+@app.route("/create_team", methods=["GET", "POST"])
+def create_team():
+    if request.method == "POST":
+        team = Team(team_name=request.form.get("team_name"))
+        db.session.add(team)
+        db.session.commit()
+        return redirect(url_for("teams"))
+    return render_template("create_team.html")
