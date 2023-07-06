d589c766f355a688a15067f953b83a404558cc1a
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-07-06 15:24:29 +0000

    Add players page layout css

--- torquay_sunday_league/static/js/script.js
+++ torquay_sunday_league/static/js/script.js
@@ -13,4 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let selects = document.querySelectorAll('select');
     M.FormSelect.init(selects);
+
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
   });
