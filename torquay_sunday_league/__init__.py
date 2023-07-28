d8cd1d51428d0266b9aa8f57ad49d6cbd9c69d5e
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-07-28 12:43:58 +0000

    Remove indent

--- torquay_sunday_league/__init__.py
+++ torquay_sunday_league/__init__.py
@@ -7,7 +7,7 @@ if os.path.exists("env.py"):
 # configure app
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
